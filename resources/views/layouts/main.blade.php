cc29ac4be73b201eb99a478e15d608422a5bfe1c
Zain ul hassan <zaniimirzaa@gmail.com>
2021-06-02 17:50:55 +0100

    02/06/2021 Added the styles and Crud Functionality to Students controller

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,4 +1,3 @@
-
 <!doctype html>
 <html lang="en">
   <head>
@@ -10,7 +9,12 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="/css/main.css">
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <title>Zcbm Invoices Portal</title>
+    
+    <!-- Font Awesome JS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+    
+    @yield("page-head")  
+    
   </head>
   <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -21,7 +25,7 @@
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
           <li class="nav-item active">
-            <a class="nav-link" href="{{ url('/home') }}">cources <span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="{{ Route('InvoiceIndex') }}">cources <span class="sr-only">(current)</span></a>
           </li>
           <li class="nav-item">
             <div class="btn-group">
@@ -30,9 +34,9 @@
               </a>
               
                 <div class="dropdown-menu">
-                  <a class="dropdown-item" href="{{ url('/addstudent') }}">Create Students</a>
+                  <a class="dropdown-item" href="{{Route('CreateStudent')}}">Create Students</a>
                   <div class="dropdown-divider"></div>
-                  <a class="dropdown-item" href="{{url('/student')}}">View Students</a>
+                  <a class="dropdown-item" href="{{Route('StudentIndex')}}">View Students</a>
                   {{-- <div class="dropdown-divider"></div>
                   {{-- <a class="dropdown-item" href="#"></a>
                   <div class="dropdown-divider"></div> --}} 
@@ -68,8 +72,21 @@
         </ul>
       </div>
     </nav>
-    <div class="container">
+    
+      <div class="container-fluid" style="padding: 1rem">
+        <form>
+          {{-- <button type="button"  class="btn btn-info btn-sm back" onclick="history.back()">
+            
+            Back
+          </button> --}}
+          <a href="javascript:window.history.back();" class=""><i class="bi bi-arrow-left-circle" style="font-size:3rem;color:deepskyblue"></i></a>
+         </form>
+      </div>
+      
+    <div class="container"  style="max-width: 1174px;">
       <div class="col-md-12">
+        
+        
         @yield('main-body')
       </div>        
     </div>
